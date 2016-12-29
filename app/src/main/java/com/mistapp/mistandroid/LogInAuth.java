9d62ad23c7f066137d6b57bb6a0b1b1d32641a3d
Hasan Qadri <hqadri6@gatech.edu>
2016-12-29 01:11:39 -0500

    Working registration activity, users are authenticated with Firebase

---
+++ app/src/main/java/com/mistapp/mistandroid/LogInAuth.java
@@ -0,0 +1,13 @@
+package com.mistapp.mistandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_auth);
+    }
+}
