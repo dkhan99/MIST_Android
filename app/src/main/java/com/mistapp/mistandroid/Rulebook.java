877caa7bd483c0e5fe30ec4abaa9166294a83d88
Hasan Qadri <hqadri6@gatech.edu>
2016-12-30 04:21:47 -0500

    Registration and sign-in auth with Firebase works, rulebook can be downloaded, and changed color scheme to MIST colors.

--- app/src/main/java/com/mistapp/mistandroid/Rulebook.java
+++ app/src/main/java/com/mistapp/mistandroid/Rulebook.java
@@ -1,20 +1,9 @@
 package com.mistapp.mistandroid;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class Rulebook extends AppCompatActivity {
 
@@ -22,62 +11,27 @@ public class Rulebook extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_rulebook);
-        File fileBrochure = new File(Environment.getExternalStorageDirectory().getPath() + "/" + "MISTRulebook2017.pdf");
-        if (!fileBrochure.exists()) {
-            CopyAssetsbrochure();
-        }
-
-        /** PDF reader code */
-        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/" + "MISTRulebook2017.pdf");
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(file), "application/pdf");
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        setContentView(R.layout.activity_rulebook);
+        copyAssets();
+        /**String path = Environment.getExternalStorageDirectory()+"/Download/MISTRulebook2017.pdf";
+        File file = new File(path);
+        Intent target = new Intent(Intent.ACTION_VIEW);
+        target.setDataAndType(Uri.fromFile(file), "application/pdf");
+        target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        Intent intent = Intent.createChooser(target, "Open File");
         try {
-            getApplicationContext().startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, "NO Pdf Viewer", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        } catch(ActivityNotFoundException e) {
+            Toast.makeText(Rulebook.this, "Install a pdf reader", Toast.LENGTH_SHORT).show();
+
         }
+    **/
     }
 
-    //method to write the PDFs file to sd card
-    private void CopyAssetsbrochure() {
-        AssetManager assetManager = getAssets();
-        String[] files = null;
-        try {
-            files = assetManager.list("");
-        } catch (IOException e) {
-            Log.e("tag", e.getMessage());
-        }
-        for (int i = 0; i < files.length; i++) {
-            String fStr = files[i];
-            if (fStr.equalsIgnoreCase("MISTRulebook2017.pdf")) {
-                InputStream in = null;
-                OutputStream out = null;
-                try {
-                    in = assetManager.open(files[i]);
-                    out = new FileOutputStream(Environment.getExternalStorageDirectory() + "/" + files[i]);
-                    copyFile(in, out);
-                    in.close();
-                    in = null;
-                    out.flush();
-                    out.close();
-                    out = null;
-                    break;
-                } catch (Exception e) {
-                    Log.e("tag", e.getMessage());
-                }
-            }
-        }
-    }
-
-    private void copyFile(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[1024];
-        int read;
-        while ((read = in.read(buffer)) != -1) {
-            out.write(buffer, 0, read);
-        }
+    /**
+     * Not the best solution, but definitely the easiest, allows user to download the pdf onto their phone
+     */
+    private void copyAssets() {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://static1.squarespace.com/static/5670ede7a976af3e2f3af0af/t/5862bd9aff7c506c99529c04/1482866208834/MISTRulebook2017.pdf")));
     }
 }
