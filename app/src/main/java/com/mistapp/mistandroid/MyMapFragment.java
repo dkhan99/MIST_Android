4bd55f8ff75fefadc44d3e2f5af451456f40ed62
Hasan Qadri <hqadri6@gatech.edu>
2017-01-21 19:31:10 -0500

    Google maps zooms in on UGA and markers pinpoint important buildings.

--- app/src/main/java/com/mistapp/mistandroid/MyMapFragment.java
+++ app/src/main/java/com/mistapp/mistandroid/MyMapFragment.java
@@ -4,6 +4,7 @@ package com.mistapp.mistandroid;
  * Created by aadil on 1/13/17.
  */
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,7 +12,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,19 +29,23 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MyMapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
     private GoogleMap mGoogleMap;
     private SupportMapFragment googleMapFragment;
     private GoogleApiClient mGoogleApiClient;
-
+    private View view = null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_map, container, false);
+        }
         if (googleServicesAvailable()) {
-            Toast.makeText(getActivity(), "Perfect!!!", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Zooming in on current location", Toast.LENGTH_LONG).show();
             initMap();
         } else {
             //No Google Maps Layout
@@ -72,25 +76,45 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback, Googl
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        /**if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
             return;
         }
         mGoogleMap.setMyLocationEnabled(true);
         mGoogleApiClient = new GoogleApiClient.Builder(getActivity()).addApi(LocationServices.API).addConnectionCallbacks(this).build();
-        Log.d("hi", "Got here");
-        mGoogleApiClient.connect();
-        //double lat = 33.9480;
-        //double lng = -83.3773;
-        //goToLocationZoom(33.9480, -83.3773, 16);
+        mGoogleApiClient.connect(); **/
+        double lat = 33.955466;
+        double lng = -83.374311;
+        goToLocationZoom(lat, lng, 17);
+        /**LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+         **/
 
-        //MarkerOptions options = new MarkerOptions().title("Building 1").position(new LatLng(lat,lng));
-       // mGoogleMap.addMarker(options);
+        double CaldwellLat = 33.954921;
+        double CaldwellLng = -83.375290;
+        double HeartyLat = 33.955946;
+        double HeartyLng = -83.375683;
+        double ChapelLat = 33.956499;
+        double ChapelLng = -83.375148;
+        double NorthHallLat = 33.956169;
+        double NorthHallLng = -83.372517;
+
+        MarkerOptions Caldwell= new MarkerOptions().title("Caldwell Hall").position(new LatLng(CaldwellLat,CaldwellLng));
+        MarkerOptions Hearty= new MarkerOptions().title("Hearty Field").position(new LatLng(HeartyLat,HeartyLng));
+        MarkerOptions Chapel = new MarkerOptions().title("Chapel").position(new LatLng(ChapelLat,ChapelLng));
+        MarkerOptions NorthHall = new MarkerOptions().title("North Hall Deck").position(new LatLng(NorthHallLat,NorthHallLng));
+
+        mGoogleMap.addMarker(Caldwell);
+        mGoogleMap.addMarker(Hearty);
+        mGoogleMap.addMarker(Chapel);
+        mGoogleMap.addMarker(NorthHall);
+
+
+
 
 
     }
@@ -117,17 +141,11 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback, Googl
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for Activity#requestPermissions for more details.
-                return;
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
             }
         }
-        Log.d("hi", "Got here 2");
 
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
@@ -147,10 +165,9 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback, Googl
         if(location == null){
             Toast.makeText(getActivity(), "Cant get current location", Toast.LENGTH_LONG).show();
         } else {
-            Log.d("hi", "Got here 3 ");
 
             LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
-            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 15);
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 16);
             mGoogleMap.animateCamera(update);
         }
     }
