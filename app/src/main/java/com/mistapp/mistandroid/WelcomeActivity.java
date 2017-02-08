df8bc62f0ecc4011cc699af14e2bc990bd617c5f
AadilOmar <aadilomar1@gmail.com>
2017-02-08 12:44:55 -0500

    Made small change to get notifications working again- phew

--- app/src/main/java/com/mistapp/mistandroid/WelcomeActivity.java
+++ app/src/main/java/com/mistapp/mistandroid/WelcomeActivity.java
@@ -42,7 +42,6 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnTouchLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        FirebaseApp.initializeApp(this);
 
         SharedPreferences sharedPref = getSharedPreferences(getString(R.string.app_package_name), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
