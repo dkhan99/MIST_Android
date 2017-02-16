ff9b4b8e31a4a1d4bba9fd078dcb2c58206fb3bb
AadilOmar <aadilomar1@gmail.com>
2017-02-16 02:28:24 -0500

    changed event object's field from duration to endTime

--- app/src/main/java/com/mistapp/mistandroid/model/Event.java
+++ app/src/main/java/com/mistapp/mistandroid/model/Event.java
@@ -2,6 +2,7 @@ package com.mistapp.mistandroid.model;
 
 import android.content.Intent;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
@@ -15,35 +16,26 @@ public class Event implements Comparable<Event>{
     private String name;
     private String location; //building name
     private String date;     // eg: '03/17'
-    private String duration; // eg: '2hr30min'
+    private String endTime; // eg: '12:00pm'
     private ArrayList<Long> roomNumbers;
-    private String time;     // '11:20am'
+    private String startTime; // '11:20am'
 
     public Event(){
-        this.name = "name";
-        this.location = "location";
-        this.date = "date";
-        this.duration = "duration";
-        ArrayList l = new ArrayList<Long>();
-        l.add(302);
-        l.add(230);
-        this.roomNumbers = l;
-        this.time = "time";
     }
 
-    public Event(String name, String location, String date, String duration, ArrayList<Long> roomNumbers, String time){
+    public Event(String name, String location, String date, String endTime, ArrayList<Long> roomNumbers, String startTime){
         this.name = name;
         this.location = location;
         this.date = date;
-        this.duration = duration;
+        this.endTime = endTime;
         this.roomNumbers = roomNumbers;
-        this.time = time;
+        this.startTime = startTime;
     }
 
     //gets the time in minutes after midnight eg(1:00am would return 60)
     private int getParsedTime(){
-        String amPm = time.substring(time.length()-2, time.length());
-        String [] t = time.substring(0,time.length()-2).split(":");
+        String amPm = startTime.substring(startTime.length()-2, startTime.length());
+        String [] t = startTime.substring(0,startTime.length()-2).split(":");
         int hour = Integer.parseInt(t[0]);
         if (amPm.equals("pm")){
             hour += 12;
@@ -77,16 +69,16 @@ public class Event implements Comparable<Event>{
         return date;
     }
 
-    public String getDuration() {
-        return duration;
+    public String getEndTime() {
+        return endTime;
     }
 
     public ArrayList<Long> getRoomNumbers() {
         return roomNumbers;
     }
 
-    public String getTime() {
-        return time;
+    public String getStartTime() {
+        return startTime;
     }
 
     public void setName(String name){
@@ -129,4 +121,36 @@ public class Event implements Comparable<Event>{
 
     };
 
+    /*
+     * Prints all the fields of the object nicely
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        String newLine = System.getProperty("line.separator");
+
+        result.append( this.getClass().getName() );
+        result.append( " Object {" );
+        result.append(newLine);
+
+        //determine fields declared in this class only (no fields of superclass)
+        Field[] fields = this.getClass().getDeclaredFields();
+
+        //print field names paired with their values
+        for ( Field field : fields  ) {
+            result.append("  ");
+            try {
+                result.append( field.getName() );
+                result.append(": ");
+                //requires access to private field:
+                result.append( field.get(this) );
+            } catch ( IllegalAccessException ex ) {
+                System.out.println(ex);
+            }
+            result.append(newLine);
+        }
+        result.append("}");
+
+        return result.toString();
+    }
 }
